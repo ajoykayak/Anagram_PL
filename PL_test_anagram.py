f069d4c1f8d332e587763cae146cf0fd670e2c25
Andrea Vander Woude <ajoykayak@gmail.com>
2016-03-10 22:49:58 -0500

    Added files via upload

---
+++ PL_test_anagram.py
@@ -0,0 +1,53 @@
+from itertools import permutations
+from nltk.corpus import words
+from collections import defaultdict
+import csv
+import numpy as np
+
+
+def open_OSdict(filename):
+    word_list = []
+    with open(filename, 'r') as f:
+        word_list = [word.strip('\n') for word in f.readlines()]
+    return word_list
+
+def anagrams(word_list,directory):
+    #output file name
+    outputfile=directory+'/anagrams.txt'
+    
+    #opening dictionary file
+    dictfile=directory+'/wordsEn.txt'
+    dict_list = []
+    with open(dictfile,'r') as g:
+        dict_list=[word.strip('\r\n') for word in g.readlines()]
+    
+    #going through each word in the word_list from the OS dictionary
+    for word in word_list:
+        if len(word) >= 4: #only words that are greater than 4 characters
+            # print 'we are in the loop', word.lower()
+            word=word.lower()
+            perms = [''.join(p) for p in permutations(word)] #find all the permutations of that word
+            perms_s=set(perms)
+            perms_s.remove(word) # remove original word from permutations list
+            anas = [y for y in perms_s if y in dict_list] #anagrams
+            
+            # only write out anagrams if they have atleast one anagram
+            if len(anas) >= 1:
+                #print anas
+                #write answers to a text file
+                with open(outputfile,'a+b') as h:
+                    writer=csv.writer(h)
+                    anas.append(word)
+                    writer.writerow(anas)
+            else:
+                pass
+            pass
+
+def main():
+    filename=input("Enter the library location on your OS in single quotes:  ")
+    directory=input("Enter the directory you want the output file to go to, without the / at the end:  ")
+    start=open_OSdict(filename)
+    anagrams(start,directory)
+
+if __name__ == '__main__':
+    main()
